d63a9275b0606feb497fcfda2faeeaa6297dedb3
baby195lxl <48685934+baby195lxl@users.noreply.github.com>
2023-01-12 18:18:03 +0800

    Update and rename n1-diy-part1.sh to n1-diy-part123.sh

--- n1-diy-part123.sh
+++ n1-diy-part123.sh
@@ -15,7 +15,7 @@ sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applic
 sed -i 's/services/system/g' feeds/luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
 
 # Add additional packages
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
